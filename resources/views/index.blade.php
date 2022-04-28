899d3beb9e63e1d0cc62f7d9e15d445caee72d1d
Cristina Riv <cristinapruebas14@gmail.com>
2022-04-28 14:20:25 +0200

    prueba

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layout')
+
+<!-- <!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica Laravel</title>
+</head>
+<body>
+    <h1>Pagina de inicio de la web</h1>
+    <header>
+        <nav>
+            <a href="<?php echo route('index')?>">Inicio</a>
+            <a href="<?php echo route('saludo')?>">Saludo</a>
+            <a href="<?php echo route('contacto')?>">Contacto</a>
+        </nav>
+    </header>
+</body>
+</html> -->
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ['as'=>'index', function () {
+    return view('index');
+}]);
+
+Route::get('pagina1', ['as' => 'contacto', function(){ //ruta para nombres de paginas
+    return view('contacto');
+}]);
+
+Route::get('pagina2/{nombre?}', ['as'=>'saludo', function($nombre = "Invitado"){ //Para que se requiera {nombre}, para que si no le pasas parámetros aparezca por defecto "Invitado" {nombre?}
+    // return view('saludo', ['nombre'=> $nombre]);
+    // return view('saludo')->with(['nombre'=>$nombre]);
+    return view('saludo', compact('nombre')); //Se puede pasar parametros para las vistas de las 3 formas pero esta es la manera mas limpia de hacerlo
+}])->where('nombre', "[A-Za-z]+"); //para filtrar con regExp
+
+Route::get('paginaCris2', ['as'=> 'paginaModificable', function(){
+    return "Pagina con nombre URL modificable";
+}]);
